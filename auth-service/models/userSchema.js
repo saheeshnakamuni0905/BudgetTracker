b4b97059c57b10f975925ba5f5ef084785750f73
saheeshnakamuni0905 <kamunisaheeshna5@gmail.com>
2025-02-17 11:28:44 -0500

    started auth-service, graphQL TBD

---
+++ auth-service/models/userSchema.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    id:{ type : String, required : true},
+    name:{ type : String, required : true, timestamps: true },
+    email:{
+        type:String, 
+        required:true, 
+        unique:true, 
+        match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "Invalid email format"]
+    },
+    password:{
+        type:String, 
+        required:true, 
+        minlength:8, 
+        validate:{
+            validator: function(value){
+                return /[A-Z]/.test(value);
+            }, 
+            message : "Password must contain at least one uppercase letter"
+        },
+    },
+});
+
+module.exports = mongoose.model("User", userSchema);
